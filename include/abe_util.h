1716b6bbe47d25ae41eb39b99f17cc1803287c8f
zghh <1069308575@qq.com>
2021-11-28 10:40:00 +0000

    Add common codes.

--- include/abe_util.h
+++ include/abe_util.h
@@ -12,4 +12,14 @@ void transfer_to_element(Element *e, element_t *t);
 
 void load_curve(Element *e_curve, pairing_t *pairing);
 
+void free_Element(Element *x);
+
+element_t **init_invert_list(int n);
+
+void clear_invert_list(element_t **list, int n);
+
+Graph build_tree(int n, int *father);
+
+int get_block_length(Element *e_curve, int data_len);
+
 #endif
